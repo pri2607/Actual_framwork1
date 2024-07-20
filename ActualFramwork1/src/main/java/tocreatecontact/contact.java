25fd47ab575d6c5da583ba770f057d0c93bb653b
priya <priya@priya>
2024-07-20 09:22:43 +0530

    9th push

---
+++ ActualFramwork1/src/main/java/tocreatecontact/contact.java
@@ -0,0 +1,12 @@
+package tocreatecontact;
+
+public class contact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Actual framwork");
+
+	}
+
+}
